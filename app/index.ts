9ca53b939a87525162b497acbcfeebf9445353b2
Animeshkbiswas <animeshbiswas15122004@gmail.com>
2025-06-19 01:17:29 +0530

    Initial commit

--- app/index.ts
+++ app/index.ts
@@ -6,6 +6,7 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const app = express();
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 
 // Logging Middleware
